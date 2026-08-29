69b8491136cf32d4b529a7b29e399b40c2fcc4f1
KHUZAIFAH. A.M.K <khuzaifah812@gmail.com>
2026-08-29 11:26:02 +0300

    Add dashboard

--- config/urls.py
+++ config/urls.py
@@ -9,11 +9,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/health/', health),
     path('api/auth/', include('accounts.urls')),
+    path('accounts/', include('attendance_system.urls')),
     path('api/', include('attendance.urls')),
     path('api/', include('lectures.urls')),
     path('api/admin/', include('programmes.urls')),
     path('', TemplateView.as_view(template_name='login.html'), name='home'),
     path('student/', TemplateView.as_view(template_name='student_dashboard.html')),
     path('lecturer/', TemplateView.as_view(template_name='lecturer_dashboard.html')),
-    path('dashboard/admin/', TemplateView.as_view(template_name='admin_dashboard.html')),
+    path('dashboard/', TemplateView.as_view(template_name='admin_dashboard.html')),
 ]
